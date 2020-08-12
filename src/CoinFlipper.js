dd78fe11a1a1fa77258bfc1fdeeecb7f71c91c5a
Elco Garcia <garciaelco18@gmail.com>
2020-08-12 16:46:24 -0500

    solution for quick exercise

---
+++ src/CoinFlipper.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Coin from './Coin';
+import { Component } from 'react';
+import choice from './choice';
+
+class CoinFlipper extends Component {
+	static defaultProps = {
+		coins: [
+			{ side: 'heads', imgSrc: 'https://tinyurl.com/react-coin-heads-jpg' },
+			{ side: 'tails', imgSrc: 'https://tinyurl.com/react-coin-tails-jpg' }
+		]
+	};
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			currCoin: null,
+			nFlips: 0,
+			nHeads: 0,
+			nTails: 0
+		};
+
+		this.handleClick = this.handleClick.bind(this);
+	}
+	flipCoin() {
+		const newCoin = choice(this.props.coins);
+		this.setState((st) => {
+			return {
+				currCoin: newCoin,
+				nFlips: st.nFlips + 1,
+				nHeads: st.nHeads + (newCoin.side === 'heads' ? 1 : 0),
+				nTails: st.nTails + (newCoin.side === 'tails' ? 1 : 0)
+			};
+		});
+	}
+	handleClick() {
+		this.flipCoin();
+	}
+	render() {
+		return (
+			<div>
+				<h2> Let's FLip a Coin</h2>
+				{this.state.currCoin && <Coin info={this.state.currCoin} />}
+				<button onClick={this.handleClick}>Flip Me!</button>
+				<p>
+					Put of {this.state.nFlips} flips, there have been {this.state.nHeads}
+					{''}
+					heads and {this.state.nTails} tails.
+				</p>
+			</div>
+		);
+	}
+}
+export default CoinFlipper;
